f37e8ffbdd6484101fcc842ddd11792ec69520fa
Ruslan <ruslan56781@mail.ru>
2023-03-09 14:50:09 +0300

    git add remove dubplicate in linked list ||

---
+++ Algorithms/С++/Medium/RemoveDuplicatesFromSortedList II.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0, head); // we create fictive node
+        ListNode* prev = dummy; 
+        while (head != NULL)
+        {
+            if (head->next != NULL && head->val == head->next->val)
+            {
+                while (head->next != NULL && head->val == head->next->val)
+                    head = head->next;
+                prev->next = head->next; // add  all unique elements in prev->next,it's mean we add in head
+            }
+            else
+                prev = head;
+            head = head->next;
+        }
+        return dummy->next; // we return head
+    }
+};
